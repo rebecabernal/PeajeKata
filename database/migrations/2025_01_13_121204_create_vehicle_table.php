2aa7ac4f50e02f5f2298087960789181897e662f
rebecabernal <rebecabernalmesa@gmail.com>
2025-01-13 13:15:28 +0100

    feat: migrations

---
+++ database/migrations/2025_01_13_121204_create_vehicle_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicle', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('type_id')->constrained("vehicle_type")->onDelete("cascade");
+            $table->string('plate');
+            $table->string('total_pay');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicle');
+    }
+};
