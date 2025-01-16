2fa6405401a3ca6ba15db5666fa7e16b308e1d8a
rebecabernal <rebecabernalmesa@gmail.com>
2025-01-16 12:02:13 +0100

    feat: api routes add

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TollController;
+use App\Http\Controllers\Api\VehicleController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post("/tolls", [TollController::class, "store"])->name("apitollstore");
+Route::post("/vehicles", [VehicleController::class, "store"])->name("apivehiclestore");
+Route::put("/vehicles/{id}/tolls/{tollId}", [VehicleController::class, "passToll"])->name("apipasstoll");
