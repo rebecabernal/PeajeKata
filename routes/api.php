92378ea59ab6a67d63d0a8c69a1d33376f529e3c
rebecabernal <rebecabernalmesa@gmail.com>
2025-01-16 12:07:32 +0100

    feat: api routes, fix: web routes

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
 Route::post("/tolls", [TollController::class, "store"])->name("apitollstore");
 Route::post("/vehicles", [VehicleController::class, "store"])->name("apivehiclestore");
 Route::put("/vehicles/{id}/tolls/{tollId}", [VehicleController::class, "passToll"])->name("apipasstoll");
