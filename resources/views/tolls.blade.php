40c81bfe06b0213eedf8aea6ecdf479d5b8be122
rebecabernal <rebecabernalmesa@gmail.com>
2025-02-03 18:09:45 +0100

    feat: all views & css, need to fix

---
+++ resources/views/tolls.blade.php
@@ -0,0 +1,22 @@
+@extends("layouts.app")
+
+@section('content')
+    <div class="title">
+        <h2>Tolls</h2>
+    </div>
+    <div class="content">
+        @foreach($tolls as $toll)
+           <div class="toll_card"> 
+                <h3>{{$toll->name}}</h3>
+                <div>
+                    <h4>Vehicles: </h4> 
+                    @foreach($toll->vehicles as $vehicle)
+                        <p>{{$vehicle->vehicleType->type}}. {{$vehicle->plate}}</p> 
+                    @endforeach
+                    <h4>Total income: </h4> 
+                    <p>${{$toll->income}}</p>
+                </div>
+           </div> 
+        @endforeach
+    </div>
+@endsection
